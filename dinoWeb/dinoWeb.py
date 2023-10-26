8ff0e19f3f3f02ff24057a478764e84f2c30d6f6
unknown <dpavon217@gmail.com>
2023-10-26 00:13:33 -0300

    creando DinoServicios

--- dinoWeb/dinoWeb.py
+++ dinoWeb/dinoWeb.py
@@ -1,11 +1,14 @@
 import openai
+import key
 
-openai.api_key = "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" 
+openai.api_key =  key.api
 
-system_rol = '''Hace de cuenta que sos un dinosaurio. Con alguna variable, te voy a indicar qué dinosaurio representas.
-                Te voy a hacer una pregunta respecto a vos (por ejemplo que comes, donde vives) 
-                y me tenés que responder como si fueras el dinosaurio en cuestión.
-                Como máximo, utilizá 200 caracteres para responder. Solo responde preguntas relacionadas al dinosaurio que representas'''
+system_rol = '''Hace de cuenta que sos un dinosaurio cuya especie te voy a pasar por parámetro con la función conversar(nombre).
+                 Apenas te hable, debes presentarte como tal y es importante que no hables de otras especies de dinosaurio 
+                 que no sea la tuya. 
+                 Te voy a hacer una pregunta respecto a vos (por ejemplo que comes, donde vives) 
+                 y me tenés que responder como si fueras la especie del dinosaurio en cuestión.
+                 Solo responde preguntas relacionadas al dinosaurio que representas.'''
         
 mensajes =[{"role": "system", "content": system_rol}]
 
@@ -14,22 +17,31 @@ class Dino:
         self.nombre = nombre
         self.descripcion = descripcion
         
-t_rex = Dino("Tiranosaurio Rex", ".")
-        
-while True:
-    user_prompt = input("Preguntame>> ")
-    mensajes.append({"role": "user", "content": user_prompt})
+class DinoServicios:
+    def __init__(self, dino: Dino):
+        self.dino = dino
     
-    if user_prompt == ".":
-        break
+    def conversar(self, nombre):
+        self.dino.nombre = nombre
+        
+        while True:
+            user_prompt = input("Preguntame>> ")
+            mensajes.append({"role": "user", "content": user_prompt})
+    
+            if user_prompt == ".":
+                break
 
-    completion = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages = mensajes,
-        max_tokens = 200
-    )
+            completion = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            messages = mensajes,
+            max_tokens = 200
+            )
         
-    respuesta = completion.choices[0].message["content"]
-    print(respuesta)
-    mensajes.append({"role": "assistant", "content": respuesta})
-    
+            respuesta = completion.choices[0].message["content"]
+            print(respuesta)
+            mensajes.append({"role": "assistant", "content": respuesta})
+            
+t_rex = Dino("Tiranosaurio Rex", ".")
+chat = DinoServicios(t_rex)
+chat.conversar(t_rex.nombre)
+   
